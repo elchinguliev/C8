e7d1c5aa2665e3e2081763be0c2b42f06b93dd0f
ASUS <ASUS@DESKTOP-O86GR9L>
2023-01-24 03:32:04 +0400

    Add project files.

---
+++ C8/Program.cs
@@ -0,0 +1,69 @@
+﻿//// C# program to display IDictionary Interface
+//using System;
+//using System.Collections.Generic;
+
+//class GFG
+//{
+
+//    // Display method with IDictionary interface
+//    static void Display(IDictionary<string, string> i)
+//    {
+//        Console.WriteLine(i["Subject"]);
+//    }
+
+//    // Driver code
+//    static void Main()
+//    {
+
+//        // Create a dictionary with student subjects
+//        Dictionary<string,
+//                   string> Student = new Dictionary<string,
+//                                                    string>();
+
+//        // Assign the value
+//        Student["Subject"] = "php";
+
+//        // Call Display method
+//        Display(Student);
+
+//        // Assign the value
+//        Student["Subject"] = "java";
+
+//        // Call Display method
+//        Display(Student);
+//    }
+//}
+
+
+
+//using System.Collections.Concurrent;
+
+//class CD_Ctor
+//{
+//    // Demonstrates:
+//    //      ConcurrentDictionary<TKey, TValue> ctor(concurrencyLevel, initialCapacity)
+//    //      ConcurrentDictionary<TKey, TValue>[TKey]
+//    static void Main()
+//    {
+//        // We know how many items we want to insert into the ConcurrentDictionary.
+//        // So set the initial capacity to some prime number above that, to ensure that
+//        // the ConcurrentDictionary does not need to be resized while initializing it.
+//        int NUMITEMS = 64;
+//        int initialCapacity = 101;
+
+//        // The higher the concurrencyLevel, the higher the theoretical number of operations
+//        // that could be performed concurrently on the ConcurrentDictionary.  However, global
+//        // operations like resizing the dictionary take longer as the concurrencyLevel rises.
+//        // For the purposes of this example, we'll compromise at numCores * 2.
+//        int numProcs = Environment.ProcessorCount;
+//        int concurrencyLevel = numProcs * 2;
+
+//        // Construct the dictionary with the desired concurrencyLevel and initialCapacity
+//        ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>(concurrencyLevel, initialCapacity);
+
+//        // Initialize the dictionary
+//        for (int i = 0; i < NUMITEMS; i++) cd[i] = i * i;
+
+//        Console.WriteLine("The square of 23 is {0} (should be {1})", cd[23], 23 * 23);
+//    }
+//}
